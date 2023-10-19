7a34c0b15559ee8d63678e7c2434088b3517e89c
Akshat Deshpande <akshatd18@outlook.com>
2023-10-19 16:57:51 +0000

    make async, fix some bugs (still non-working)

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use ozelot::serverbound::ServerboundPacket;
-use ozelot::Client;
 use ozelot::Packet;
 use ozelot::Server;
 use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::{Read, Write};
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::process::{exit, Child, Command, Stdio};
-use std::thread;
-use std::time::Duration;
+use tokio::fs::File;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+use tokio::net::TcpStream;
+use tokio::process::{Child, Command};
+use std::process::{Stdio, exit};
 
 #[derive(Deserialize, Serialize, Clone)]
 struct Config {
@@ -44,17 +42,17 @@ impl Default for Config {
     }
 }
 
-fn main() {
-    let mut file = File::options().write(true).create(true).read(true).append(true).open("config.toml")
-        .expect("your config file was unable to be created or does not exist. please make sure you have rights to create files in the current directory.");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = File::options().write(true).create(true).read(true).append(true).open("config.toml").await?;
     let mut config_string = String::new();
-    file.read_to_string(&mut config_string).unwrap();
+    file.read_to_string(&mut config_string).await?;
 
     if config_string.is_empty() {
         let string = toml::to_string_pretty(&Config::default()).unwrap();
 
-        file.write_all(string.as_bytes()).unwrap();
-        file.write(b"# the protocol version can either be left blank to force a server start, or filled using your server's version using `https://wiki.vg/Protocol_version_numbers`. Filling the protocol version is recommended because it will allow the proxy itself to reject the connection, rather than starting the server and it having to reject the clients on the wrong versions.").unwrap();
+        file.write_all(string.as_bytes()).await?;
+        file.write_all(b"# the protocol version can either be left blank to force a server start, or filled using your server's version using `https://wiki.vg/Protocol_version_numbers`. Filling the protocol version is recommended because it will allow the proxy itself to reject the connection, rather than starting the server and it having to reject the clients on the wrong versions.").await?;
         eprintln!(
             "a default config file was created. please review the file and re-run this program"
         );
@@ -73,20 +71,21 @@ fn main() {
         .spawn()
         .expect("failed to spawn child process");
 
-    proxy(&server, &config);
+    proxy(&server, &config).await?;
+    
+    Ok(())
 }
 
-fn proxy(child: &Child, config: &Config) {
-    thread::sleep(Duration::from_millis(10000));
-    println!("out of sleep!");
-    let listener = TcpListener::bind("127.0.0.1:".to_string() + &config.proxy_port.to_string())
-        .expect("failed to bind to port");
+async fn proxy(_child: &Child, config: &Config) -> Result<(), Box<dyn std::error::Error>> {
+    // thread::sleep(Duration::from_millis(10000));
+    // println!("out of sleep!");
+    let listener = TcpListener::bind("127.0.0.1:".to_string() + &config.proxy_port.to_string()).await?;
     println!("listening on {}", &config.proxy_port);
     let mut server =
-        TcpStream::connect("127.0.0.1:".to_string() + &config.server_port.to_string()).unwrap();
+        TcpStream::connect("127.0.0.1:".to_string() + &config.server_port.to_string()).await?;
 
     loop {
-        let client = listener.accept();
+        let client = listener.accept().await;
         if let Err(e) = client {
             eprintln!("failed to accept client: {}", e);
             continue;
@@ -95,14 +94,14 @@ fn proxy(child: &Child, config: &Config) {
         let (stream, address) = client.unwrap();
         println!("new client connected from: {}", address);
 
-        let mut client = Server::from_tcpstream(stream).unwrap();
+        let mut client = Server::from_tcpstream(stream.into_std()?).unwrap();
         let client_packets = client.read().unwrap();
 
         for packet in client_packets {
             match packet {
                 ServerboundPacket::Handshake(ref p) => {
                     if &config.protocol_ver == p.get_protocol_version() {
-                        server.write_all(p.to_u8().unwrap().as_slice()).unwrap();
+                        server.write_all(p.to_u8().unwrap().as_slice()).await?;
                     } else {
                         client.close().unwrap();
                         continue;
@@ -112,7 +111,7 @@ fn proxy(child: &Child, config: &Config) {
                 _ => {
                     server
                         .write_all(packet.to_u8().unwrap().as_slice())
-                        .unwrap();
+                        .await?;
                     dbg!(packet);
                 }
             }
